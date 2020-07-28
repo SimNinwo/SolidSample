8db2a9d8f9d8255f9ed7e45f2e7a7fdfb6f15cc0
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-07-28 13:45:54 +0100

    Resolving null LSP(listov Substitution Principle) violation

--- ArdalisRating/RaterFactory.cs
+++ ArdalisRating/RaterFactory.cs
@@ -1,39 +1,23 @@
-﻿namespace ArdalisRating
+﻿using System;
+
+namespace ArdalisRating
 {
     public class RaterFactory
     {
         public Rater Create(Policy policy, RatingEngine engine)
         {
-            switch (policy.Type)
+            try
             {
-                case PolicyType.Auto:
-                    return new AutoPolicyRater(engine, engine.Logger);
+                //**Using Reflection to instantiate * **//
 
-                case PolicyType.Flood:
-                    return new FloodPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Land:
-                    return new LandPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Life:
-                    return new LifePolicyRater(engine, engine.Logger);
-
-                default:
-                    // currently this can't be reached
-                    return new UnknownPolicyRater(engine, engine.Logger);
+                return (Rater)Activator.CreateInstance(
+                    Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"),
+                    new object[] { engine, engine.Logger });
             }
-
-
-            //try
-            //{
-            //  \\**USing Reflection to instantiate***\\
-            //    return (Rater)Activator.CreateInstance(Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"),
-            //        new object[] { engine, engine.Logger });
-            //}
-            //catch
-            //{
-            //    return null;
-            //}
+            catch
+            {
+                return new UnknownPolicyRater(engine, engine.Logger);
+            }
 
         }
     }
